68a86cdc59ff8df7a36937ad19da2c8ce993e71f
akhilcvc <84622485+akhilcvc@users.noreply.github.com>
2021-05-30 08:33:41 +0530

    Update stream.py

--- WebStreamer/bot/plugins/stream.py
+++ WebStreamer/bot/plugins/stream.py
@@ -103,7 +103,7 @@ async def channel_receive_handler(bot, broadcast):
             message_id=broadcast.message_id,
             reply_markup=InlineKeyboardMarkup(
                 [
-                    [InlineKeyboardButton("💫Direct Download Link", url=f"https://t.me/AH_File2Link_Bot?start=AbirHasan2005_{str(log_msg.message_id)}")]
+                    [InlineKeyboardButton("💫Direct Download Link", url=f"https://t.me/Filesto_Link_Bot?start=FILMWORLDOFFICIA_{str(log_msg.message_id)}")]
                 ]
             )
         )
